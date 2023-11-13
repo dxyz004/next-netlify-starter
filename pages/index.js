07bb26a1515fcf451aaaffab6d3253dba0642b36
d_n89 <d_n89@txstate.edu>
2023-11-13 16:22:36 -0600

    notes link

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ export default function Home() {
       <header className="vh-100 bg-black dt w-100">
         <CubeWireframe />
         <div className="absolute w-100 tc" style={{top: '50%', transform: 'translateY(-50%)'}}>
-        <a class="link white f2 dib mr3" href="https://www.youtube.com/watch?v=pP1uswAwZi4" target="_blank" rel="noopener noreferrer">Demo Video</a>
-        <a class="link white f2 dib mr3" href="https://github.com/dxyz004" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a class="link white f3 dib mr3" href="https://www.youtube.com/watch?v=pP1uswAwZi4" target="_blank" rel="noopener noreferrer">Demo Video</a>
+        <a class="link white f3 dib mr3" href="https://github.com/dxyz004" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a class="link white f3 dib mr3" href="https://heuristic-brahmagupta-965a2c.netlify.app/" target="_blank" rel="noopener noreferrer">Notes</a>
         </div>
       </header>
     </article>
