0682957418909809fbd7c97de0dae3cc3f332ee0
dxyz004 <danielmnguyenmail@gmail.com>
2024-05-27 20:27:54 -0500

    revert blog link

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import CubeWireframe from '@components/CubeFrame'
-import ParticleRing from '@components/ParticleRing'
-import MobiusStrip from '../components/MobiusStrip'
 
 export default function Home() {
   return (
@@ -13,7 +11,7 @@ export default function Home() {
         <div className="absolute w-100 tc" style={{top: '50%', transform: 'translateY(-50%)'}}>
         <a class="link white f3 dib mr3" href="https://www.youtube.com/watch?v=pP1uswAwZi4" target="_blank" rel="noopener noreferrer">Demo Video</a>
         <a class="link white f3 dib mr3" href="https://github.com/dxyz004" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a class="link white f3 dib mr3" href="https://danielsnotes.net/blog" target="_blank" rel="noopener noreferrer">Notes</a>
+        <a class="link white f3 dib mr3" href="https://exquisite-starlight-fbe91d.netlify.app/" target="_blank" rel="noopener noreferrer">Notes</a>
         </div>
       </header>
     </article>
